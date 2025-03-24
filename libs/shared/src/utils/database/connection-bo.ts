3006c553957f2636b67f402cef7520f97ae9b7a4
medelkouch <mohammed.elkouch@uit.ac.ma>
2025-03-24 13:09:00 +0000

    feat: implement tenant-specific database connection modules for backoffice and client

---
+++ libs/shared/src/utils/database/connection-bo.ts
@@ -0,0 +1,35 @@
+import { AllBackofficeEntities, getBoSourceOptions } from '@lib/shared';
+import { NotFoundException } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+
+export async function getTenantConnectionBO(
+  schema_name: string,
+): Promise<DataSource> {
+  try {
+    // Create a new DataSource instance
+    const newDataSource = new DataSource({
+      ...getBoSourceOptions(),
+      entities: AllBackofficeEntities,
+      migrations: undefined,
+      name: schema_name,
+      schema: schema_name,
+      poolSize: 1,
+    });
+    console.log('Initializing connection...', newDataSource.options);
+    await newDataSource.initialize();
+    console.log('+++ schema initialized +++');
+    await newDataSource.query(`CREATE SCHEMA IF NOT EXISTS "${schema_name}"`);
+    await newDataSource.query(`SET search_path TO "${schema_name}"`);
+    const result = await newDataSource.query<{ schema: string }[]>(
+      'SELECT current_schema() as schema',
+    );
+    if (result[0].schema !== schema_name) {
+      throw new NotFoundException(`Schema "${schema_name}" not found`);
+    }
+    return newDataSource;
+  } catch (error) {
+    console.log('Error creating connection', error);
+    console.error(error);
+    throw new NotFoundException(`Schema "${schema_name}" not found`);
+  }
+}
