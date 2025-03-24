8e1277269e20204ec126e871e0e1be88195b0bf9
medelkouch <mohammed.elkouch@uit.ac.ma>
2025-03-24 10:20:54 +0000

    feat: add event emitter support for clinic events and migration handling

---
+++ src/apps/backoffice/features/clinics/events/clinic.event.ts
@@ -0,0 +1,34 @@
+import { ActivationStatus, SchemaMigrationStatus } from '@lib/shared';
+import { Injectable } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+
+import { ClinicEvents } from './clinic-events';
+
+@Injectable()
+export class ClinicEvent {
+  constructor(private readonly eventEmitter: EventEmitter2) {}
+
+  emitClinicCreationUpdate(clinicId: string): void {
+    this.eventEmitter.emit(ClinicEvents.ClinicCreationUpdated, {
+      clinicId,
+    });
+  }
+
+  emitMigrationStarted(clinicId: string): void {
+    this.eventEmitter.emit(ClinicEvents.MigrationStarted, {
+      clinicId,
+    });
+  }
+
+  emitMigrationSuccess(clinicId: string): void {
+    this.eventEmitter.emit(ClinicEvents.MigrationSuccess, {
+      clinicId,
+    });
+  }
+
+  emitMigrationFailed(clinicId: string): void {
+    this.eventEmitter.emit(ClinicEvents.MigrationFailed, {
+      clinicId,
+    });
+  }
+}
