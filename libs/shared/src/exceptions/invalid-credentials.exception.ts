a3eb5692568a1e9d52b6ea00f65c529481295b55
Faye79-dev <mohamed.f@wiggli.io>
2025-04-07 00:44:41 +0100

    feat: add custom exception filter

---
+++ libs/shared/src/exceptions/invalid-credentials.exception.ts
@@ -0,0 +1,11 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { ExceptionErrorType } from '../types';
+
+export class InvalidCredentialsException extends UnauthorizedException {
+    constructor() {
+        super({
+            error_code: ExceptionErrorType.InvalidCredentials,
+            message: 'Invalid credentials',
+        });
+    }
+}
