0b2d9628aa3a52a3945ae2a5005587a3bda97888
Faye79-dev <mohamed.f@wiggli.io>
2025-04-07 00:45:25 +0100

    feat: add auth service and dtos

---
+++ src/apps/client/features/auth/dtos/index.ts
@@ -0,0 +1,4 @@
+import { ClientUserLoginDto } from './user-login.client.dto';
+import { ClientUserLoginResponseDto } from './user-login-response.client.dto';
+
+export { ClientUserLoginDto, ClientUserLoginResponseDto }
---
+++ src/apps/client/features/auth/services/auth.client.service.ts
@@ -0,0 +1,57 @@
+import { ClientUserEntity, InvalidCredentialsException } from "@lib/shared";
+import { BadRequestException, Inject, Injectable, Scope } from "@nestjs/common";
+import { CLIENT_CONNECTION } from "src/apps/client/constants/app.constant";
+import { DataSource, Repository } from "typeorm";
+import { ClientUserLoginDto, ClientUserLoginResponseDto } from "../dtos";
+import { TClientJwtPayload } from "../types";
+import { ClientUserMapper } from "../../users/users.client.mapper";
+import { JWTAuthHelper } from "@lib/shared/modules/jwt-auth/jwt-auth.helper";
+import { ConfigService } from "@nestjs/config";
+import { ClientUserService } from "../../users/services/users.client.service";
+import { REQUEST } from "@nestjs/core";
+import { ExceptionErrorType } from "@lib/shared/types";
+
+@Injectable({ scope: Scope.REQUEST })
+export class ClientAuthService {
+    private readonly clientUserRepository: Repository<ClientUserEntity>;
+
+    constructor(
+        @Inject(REQUEST) private request: Request,
+        private readonly configService: ConfigService,
+        @Inject(CLIENT_CONNECTION) connection: DataSource,
+        private readonly clientUserService: ClientUserService,
+        private readonly jwtAuthHelper: JWTAuthHelper<TClientJwtPayload>
+    ) {
+        this.clientUserRepository = connection.getRepository(ClientUserEntity);
+    }
+
+    async login({ email, password, remember_me }: ClientUserLoginDto): Promise<ClientUserLoginResponseDto> {
+        const tenantId = this.request.headers['x-tenant-id']
+        if (!tenantId)
+            throw new BadRequestException({
+                error_code: ExceptionErrorType.TenantIsRequired,
+                message: "Tenant ID must be provided",
+            });;
+
+        const user = await this.clientUserRepository.findOneBy({ email });
+        if (!user) throw new InvalidCredentialsException();
+
+        const passwordMatched = await user.checkPassword(password);
+        if (!passwordMatched) throw new InvalidCredentialsException();
+
+        // check user account status
+        this.clientUserService.checkUserStatus(user)
+
+        // generate token
+        const secret = this.configService.getOrThrow<string>('JWT_AUTH_SECRET')
+        const expiresIn = remember_me ?
+            this.configService.getOrThrow<string>('REMEMBER_ME_ACCESS_TOKEN_EXPIRES_IN') :
+            this.configService.getOrThrow<string>('ACCESS_TOKEN_EXPIRES_IN');
+        const payload: TClientJwtPayload = { userId: user.id, tenantId };
+        const token = this.jwtAuthHelper.generateAccessToken({ payload, secret: secret!, expiresIn: expiresIn! });
+
+        const clientUserMapper = new ClientUserMapper()
+        const userDto = clientUserMapper.toDtoWithRelations(user)
+        return { user: userDto, tokens: { accessToken: token } };
+    }
+}
