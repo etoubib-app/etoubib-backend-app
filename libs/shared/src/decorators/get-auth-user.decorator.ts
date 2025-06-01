c731d0740050e16a57196d0c984d1f02432b1745
aymankaddioui <aymankaddioui@gmail.com>
2025-06-01 06:35:03 +0100

    feat: add decorators for retrieving authenticated user and tenant ID

--- libs/shared/src/decorators/get-auth-user.decorator.ts
+++ libs/shared/src/decorators/get-auth-user.decorator.ts
@@ -1,12 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { TBoAuthUser } from 'src/apps/backoffice/features/auth/types';
-import { TClientAuthUser } from 'src/apps/client/features/auth/types';
 
-type TAuthUser = TClientAuthUser & TBoAuthUser;
+import { TAuthUser } from '../modules';
+
 export const GetAuthUser = createParamDecorator(
   (data: keyof TAuthUser | undefined, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-    const user = request.user as TAuthUser;
+    const request = context.switchToHttp().getRequest<{ user: TAuthUser }>();
+    const user = request.user;
 
     if (!user) return null;
     return data ? user?.[data] : user;
