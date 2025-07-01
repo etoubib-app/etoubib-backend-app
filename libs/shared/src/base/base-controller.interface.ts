8b87a000f867029d6025212e326b917476b741ee
Faye79-dev <mohamed.f@wiggli.io>
2025-07-01 15:14:17 +0100

    feat: add IBaseController interface for CRUD operations and optional restore method

---
+++ libs/shared/src/base/base-controller.interface.ts
@@ -0,0 +1,16 @@
+import { DeepPartial } from "typeorm";
+import { PaginationQueryDto } from "../dto";
+import { TPaginatedData } from "../types";
+
+export interface IBaseController<
+  Entity extends { id: string, deletedAt: Date },
+  CreateDto extends DeepPartial<Entity>,
+  UpdateDto extends DeepPartial<Entity>,
+> {
+  findAll<TQuery extends PaginationQueryDto>(query: TQuery): Promise<TPaginatedData<Entity>>;
+  findOne(id: string): Promise<Entity>;
+  create(dto: CreateDto): Promise<Entity>;
+  update(id: string, dto: UpdateDto): Promise<Entity>;
+  delete(id: string): Promise<void>;
+  restore?(id: string): Promise<Entity>;
+}
--- libs/shared/src/base/index.ts
+++ libs/shared/src/base/index.ts
@@ -1,3 +1,6 @@
 export * from './base.entity';
 export * from './base.mapper';
 export * from './base-response.dto';
+export * from './services/base.service';
+export * from './services/soft-delete-base.service';
+export * from './base-controller.interface';
