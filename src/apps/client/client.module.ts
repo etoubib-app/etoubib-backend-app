401a367152b6d10e20255a66a6ecb87c110ee21f
Faye79-dev <mohamed.f@wiggli.io>
2025-07-01 22:37:53 +0100

    feat: add PatientModule and AddressModule to ClientModule

--- src/apps/client/client.module.ts
+++ src/apps/client/client.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { ClientUserModule } from './features/users/users.client.module';
 import { ClientAuthModule } from './features/auth/auth.client.module';
+import { PatientModule } from './features/patient/patient.client.module';
+import { AddressModule } from './modules/address/address.client.module';
 
 @Module({
-  imports: [ClientUserModule, ClientAuthModule],
+  imports: [ClientUserModule, ClientAuthModule, PatientModule, AddressModule],
 })
-export class ClientModule {}
+export class ClientModule { }
---
+++ src/apps/client/modules/address/dtos/index.ts
@@ -0,0 +1,4 @@
+import { CreateAddressDto, } from './create-address.client.dto';
+import { AddressResponseDto, } from './address-response.client.dto';
+
+export { CreateAddressDto, AddressResponseDto };
