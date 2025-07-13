b8b6bae13faecc4a69812b1fd04e8486e6855789
Faye79-dev <mohamed.f@wiggli.io>
2025-07-14 00:09:26 +0100

    feat: add FormModule to ClientModule imports

--- src/apps/client/client.module.ts
+++ src/apps/client/client.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { ClientAuthModule } from './features/auth/auth.client.module';
 import { PatientModule } from './features/patient/patient.client.module';
 import { AddressModule } from './modules/address/address.client.module';
 import { ClientUserModule } from './features/users/users.client.module';
+import { FormModule } from './features/form/form.client.module';
+import { ClientAuthModule } from './features/auth/auth.client.module';
 
-// TODO: merge!!
 @Module({
-  imports: [ClientUserModule, ClientAuthModule, PatientModule, AddressModule],
+  imports: [ClientAuthModule, ClientUserModule, PatientModule, AddressModule, FormModule],
 })
 export class ClientModule { }
