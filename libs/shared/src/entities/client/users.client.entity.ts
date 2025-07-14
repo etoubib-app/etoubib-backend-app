8cff8578ab4e22792b3801ba63ba1c1ace8930f1
Faye79-dev <mohamed.f@wiggli.io>
2025-07-14 22:12:32 +0100

    refactor: rename ClientUser to User and hide password in query select

--- libs/shared/src/entities/client/users.client.entity.ts
+++ libs/shared/src/entities/client/users.client.entity.ts
@@ -1,17 +1,13 @@
-import {
-  ClientUserStatus,
-  type TClientUserStatus,
-} from '@lib/shared/enums/client';
+import { UserStatus, type TUserStatus } from '@lib/shared/enums/client';
 import { bycryptHashPassword } from '@lib/shared/helpers';
 import { UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 import { Exclude } from 'class-transformer';
 import { Column, Entity } from 'typeorm';
-
 import { BaseEntity } from '../../base';
 
 @Entity('users')
-export class ClientUser extends BaseEntity {
+export class User extends BaseEntity {
   @Column({
     type: 'varchar',
     name: 'first_name',
@@ -33,17 +29,18 @@ export class ClientUser extends BaseEntity {
     type: 'varchar',
     name: 'password',
     length: 255,
+    select: false,
   })
   @Exclude()
   private _password!: string;
 
-  @Column({ type: 'varchar', default: ClientUserStatus.active })
-  status!: TClientUserStatus;
+  @Column({ type: 'varchar', default: UserStatus.active })
+  status!: TUserStatus;
 
   @Column({ type: 'boolean', default: false })
   isOwner?: boolean;
 
-  constructor(data: Partial<ClientUser> = {}) {
+  constructor(data: Partial<User> = {}) {
     super();
     Object.assign(this, data);
   }
@@ -60,11 +57,16 @@ export class ClientUser extends BaseEntity {
     return await bcrypt.compare(plainPassword, this._password);
   }
 
+  toSafeObject(): Omit<this, '_password'> {
+    const { _password, ...rest } = this as any; // TODO: fix type assertion
+    return rest;
+  }
+
   checkUserStatus(): boolean {
-    if (this.status == ClientUserStatus.inactive) {
+    if (this.status == UserStatus.inactive) {
       throw new UnauthorizedException('User inactive not authorized to login');
     }
-    if (this.status == ClientUserStatus.blocked) {
+    if (this.status == UserStatus.blocked) {
       throw new UnauthorizedException('User blocked not authorized to login');
     }
     return true;
