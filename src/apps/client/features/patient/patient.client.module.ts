5232d925c4e84053423719e2c37707193518b91b
Faye79-dev <mohamed.f@wiggli.io>
2025-07-14 19:41:48 +0100

    feat: implement PatientFormApiService for handling patient form submissions and retrieval of answers

--- src/apps/client/features/patient/patient.client.module.ts
+++ src/apps/client/features/patient/patient.client.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ClientDatabaseModule } from '@lib/shared/modules';
 import { PatientsController } from './patient.client.controller';
-import { PatientsApiService } from './services/patients-api.client.service';
+import { PatientApiService } from './services/patient-api.client.service';
 import { JWTAuthModule } from '@lib/shared/modules/jwt-auth';
+import { PatientFormApiService } from './services/patient-form-api.client.service';
 
 @Module({
   imports: [ClientDatabaseModule, JWTAuthModule],
   controllers: [PatientsController],
-  providers: [PatientsApiService],
+  providers: [PatientApiService, PatientFormApiService],
   exports: [],
 })
 export class PatientModule { }
