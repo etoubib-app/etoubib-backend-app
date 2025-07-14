b199d20f2883883fea72a912f963104257dd1986
Faye79-dev <mohamed.f@wiggli.io>
2025-07-14 19:43:34 +0100

    feat: add fillPatientForm and getPatientFormAnswers endpoints

--- src/apps/client/features/patient/patient.client.controller.ts
+++ src/apps/client/features/patient/patient.client.controller.ts
@@ -2,31 +2,36 @@ import { ApiResponseWithData, ClientController, PatientEntity } from '@lib/share
 import { Body, Delete, Get, HttpCode, HttpStatus, Injectable, Param, ParseUUIDPipe, Patch, Post, Query, Scope, UseGuards } from '@nestjs/common';
 
 import { ApiConflictResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { PatientsApiService } from './services/patients-api.client.service';
+import { PatientApiService } from './services/patient-api.client.service';
 import { IBaseCRUDController } from '@lib/shared/base/base-controller.interface';
 import { CreatePatientDto, PatientResponseDto, UpdatePatientDto } from './dtos';
 import { PaginationQueryDto } from '@lib/shared/dto';
 import { ClientJwtAuthGuard } from '@lib/shared/modules/jwt-auth';
+import { FillFormDto, FormAnswersResponseDto } from '../form/dtos';
+import { PatientFormApiService } from './services/patient-form-api.client.service';
 
 @ApiTags('Patients')
 @UseGuards(ClientJwtAuthGuard)
 @Injectable({ scope: Scope.REQUEST })
 @ClientController('patients')
 export class PatientsController implements IBaseCRUDController<PatientEntity, CreatePatientDto, UpdatePatientDto> {
-  constructor(protected readonly patientsApiService: PatientsApiService) { }
+  constructor(
+    protected readonly patientApiService: PatientApiService,
+    protected readonly patientFormApiService: PatientFormApiService
+  ) { }
 
   @Get()
   @ApiOperation({ description: 'Get all patients' })
   @ApiResponseWithData(PatientResponseDto, { isArray: true })
   findAll(@Query() query: PaginationQueryDto) {
-    return this.patientsApiService.findAll(query);
+    return this.patientApiService.findAll(query);
   }
 
   @Get(':id')
   @ApiOperation({ description: 'Get patient by id' })
   @ApiResponseWithData(PatientResponseDto)
   findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.patientsApiService.findOne(id);
+    return this.patientApiService.findOne(id);
   }
 
   @Post()
@@ -34,21 +39,21 @@ export class PatientsController implements IBaseCRUDController<PatientEntity, Cr
   @ApiResponseWithData(PatientResponseDto, { status: HttpStatus.CREATED })
   @ApiConflictResponse({ description: 'Patient already exists' })
   create(@Body() dto: CreatePatientDto) {
-    return this.patientsApiService.create(dto);
+    return this.patientApiService.create(dto);
   }
 
   @Patch(':id')
   @ApiResponseWithData(PatientResponseDto)
   @ApiOperation({ description: 'Update patient' })
   update(@Param('id', ParseUUIDPipe) id: string, @Body() dto: UpdatePatientDto) {
-    return this.patientsApiService.update(id, dto);
+    return this.patientApiService.update(id, dto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  @ApiOperation({ description: 'Delete patient' })
+  @ApiOperation({ description: 'Soft delete patient' })
   async softDelete(@Param('id', ParseUUIDPipe) id: string) {
-    return this.patientsApiService.remove(id)
+    return this.patientApiService.remove(id)
   }
 
   @Post('restore/:id')
@@ -56,6 +61,29 @@ export class PatientsController implements IBaseCRUDController<PatientEntity, Cr
   @ApiResponseWithData(PatientResponseDto)
   @ApiOperation({ description: 'Restore deleted patient' })
   async restore(@Param('id', ParseUUIDPipe) id: string) {
-    return this.patientsApiService.restore(id)
+    return this.patientApiService.restore(id)
   }
+
+  // TODO: define swagger response DTO
+  @Post(':patientId/forms/:formId/answers')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ description: 'Fill a form for a patient' })
+  async fillPatientForm(
+    @Body() dto: FillFormDto,
+    @Param('formId', ParseUUIDPipe) formId: string,
+    @Param('patientId', ParseUUIDPipe) patientId: string
+  ) {
+    return await this.patientFormApiService.fillPatientForm(patientId, formId, dto);
+  }
+
+  @Get(':patientId/forms/:formId/answers')
+  @ApiResponseWithData(FormAnswersResponseDto)
+  @ApiOperation({ description: 'Get patient form answers' })
+  async getPatientFormAnswers(
+    @Param('formId', ParseUUIDPipe) formId: string,
+    @Param('patientId', ParseUUIDPipe) patientId: string
+  ) {
+    return await this.patientFormApiService.getPatientFormAnswers(patientId, formId);
+  }
+
 }
